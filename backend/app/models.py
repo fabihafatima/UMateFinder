ccb2ff53bdd1ac7afe098668192363ce7d421ae4
Devyani Goil <goildevyani1@gmail.com>
2024-11-09 13:06:36 -0500

    Editing Models

--- backend/app/models.py
+++ backend/app/models.py
@@ -4,7 +4,7 @@ from app import mongo
 #     return mongo.db.items.find_one({"name": name})
 
 def find_user_data(email):
-    return mongo.db.user_profile_data.find_one({"email": email})
+    return mongo.bumbleForRoommates.user_profile_data.find_one({"email": email})
 
 def find_login_data(email):
-    return mongo.db.user_login_data.find_one({"email": email})
+    return mongo.bumbleForRoommates.user_login_data.find_one({"email": email})

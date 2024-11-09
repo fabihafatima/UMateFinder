b1a6b7a6319da08fdffd2032e9bd0a0b3b4cf991
Devyani Goil <goildevyani1@gmail.com>
2024-11-09 13:02:13 -0500

    adding mongo model

--- backend/app/models.py
+++ backend/app/models.py
@@ -3,5 +3,8 @@ from app import mongo
 # def find_item(name):
 #     return mongo.db.items.find_one({"name": name})
 
-def find_preferences(email):
-    return mongo.db.user_info.find_one({"email": email})
+def find_user_data(email):
+    return mongo.db.user_profile_data.find_one({"email": email})
+
+def find_login_data(email):
+    return mongo.db.user_login_data.find_one({"email": email})

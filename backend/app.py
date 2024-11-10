05d0c90cb373e3e75b5897107cae6316e0e7abcf
Devyani Goil <goildevyani1@gmail.com>
2024-11-10 01:54:33 -0500

    Adding CORS headers

--- backend/app.py
+++ backend/app.py
@@ -4,5 +4,15 @@ from flask_cors import CORS
 app = create_app()
 CORS(app)
 
+@app.after_request
+def add_cors_headers(response):
+    """
+    Add CORS headers to allow requests from any origin.
+    """
+    response.headers.add("Access-Control-Allow-Origin", "*")
+    response.headers.add("Access-Control-Allow-Headers", "Content-Type,Authorization")
+    response.headers.add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+    return response
+
 if __name__ == '__main__':
     app.run(debug=True)
